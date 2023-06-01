ba62ad716eee2db21268ee496c2b329859525a55
Andrii Lytvyn <litvin.a.a@gmail.com>
2023-06-01 23:15:04 +0200

    first task

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,66 @@
-import { FC } from 'react'
+import { ChangeEvent, FC, FormEvent, useState } from 'react'
 import './App.css'
+import css from "./App.module.css";
+interface Field {
+  name: string;
+  password: string;
+}
+const initFormField: Field = {
+  name: "",
+  password: ""
+}
 
-const App:FC=()=> {
-  //const [count, setCount] = useState(0)
+const App: FC = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [fields, setFields] = useState(initFormField);
+  const getInputData = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    if (name === "name") {
+      setName(value);
+    } else if (name === "password") {
+      setPassword(value)
+    }
+    setFields((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
+  const submitForm = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("fields",fields);
+    resetForm();
+  };
+
+  const resetForm = () =>{
+    setName("");
+    setPassword("");
+  }
 
   return (
     <>
-      <div>
-      <h1>Hello</h1>
+      <div className={css.div_wrapper}>
+        <form onSubmit={submitForm}>
+          <label> Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={getInputData}
+            autoComplete='off' />
+          <input type="password"
+            name="password"
+            value={password}
+            onChange={getInputData}
+            autoComplete='off'
+          />
+          <button type="submit">Add</button>
+        </form>
       </div>
-      
     </>
   )
 }
